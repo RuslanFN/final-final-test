2a48c615f231f4e1e2f839f976e7c4481e0d16f3
Ruslan Fatkulin <rusya86hmrus@gmail.com>
2024-12-23 01:08:14 +0500

    finish5

--- recipe_hub/recipe_hub/settings.py
+++ recipe_hub/recipe_hub/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e1n82refm&dofrnql)_qi#%xz@lwh)dc(d5kazn^$e*7h4war&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.localhost', '127.0.0.1', '[::1]']
 
