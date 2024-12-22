20b8db121a6808b5d86f3e86d76ac503ab92af5c
Ruslan Fatkulin <rusya86hmrus@gmail.com>
2024-12-22 23:48:22 +0500

    debug finish

--- recipe_hub/recipe_hub/settings.py
+++ recipe_hub/recipe_hub/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'recipe_hub.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'data' / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
